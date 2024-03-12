5d0cd3037e7af8cceaa7cd3c5a2677f41b0d1692
WenruiChen <719941703@qq.com>
2024-03-12 14:16:11 -0700

    databaseConnection

---
+++ PHP/connection.php
@@ -0,0 +1,13 @@
+
+<?php
+
+$host = "cosc360.ok.ubc.ca";
+$database = "db_56362320";
+$user = "56362320";
+$password = "56362320";
+
+$connection = mysqli_connect($host, $user, $password, $database);
+
+$error = mysqli_connect_error();
+
+?>
