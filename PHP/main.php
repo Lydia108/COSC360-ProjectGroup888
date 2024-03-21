ef86d36e3b13332ce1add391595f0781d459eadf
WenruiChen <719941703@qq.com>
2024-03-20 23:13:39 -0700

    post

--- PHP/main.php
+++ PHP/main.php
@@ -4,30 +4,18 @@
 <head lang="en">
     <meta charset="utf-8">
     <title>Bloggie</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../CSS/main.css">
     <script src='https://www.w3schools.cn/fonts/kit/a076d05399.js'></script>
     <script src="https://kit.fontawesome.com/d1344ce34d.js" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/0485a9f289.js" crossorigin="anonymous"></script>
-    <script>
-    function toggleLike() {
-        var like = document.getElementById("thumbsup");
-        var like1 = document.getElementById("thumbsup1");
 
-        // Toggle visibility
-        if (like.style.display === "none") {
-            like.style.display = "inline-block";
-            like1.style.display = "none";
-        } else {
-            like.style.display = "none";
-            like1.style.display = "inline-block";
-        }
-    }
-    </script>
 </head>
+<!-- profile -->
 <?php
 session_start();
-include 'connection.php'; // Make sure this path is correct
+include 'connection.php'; 
 if (isset($_SESSION['user_id'])) {
     $userId = $_SESSION['user_id'];
     $stmt = $conn->prepare("SELECT firstName, lastName, emailAddress, icon FROM user WHERE userId = ?");
@@ -39,7 +27,11 @@ if (isset($_SESSION['user_id'])) {
         $user = $result->fetch_assoc();
         $firstName = htmlspecialchars($user['firstName']);
         $lastName = htmlspecialchars($user['lastName']);
-        // echo 'Icon data length: ' . strlen((string)$user['firstName']) . ' bytes</p>';
+        if ($user['icon']) {
+            $iconData = base64_encode($user['icon']);
+        } else {
+            $iconData = ''; 
+        }   // Convert the icon data to a data URI
     } else {
         echo "No user found.";
     }
@@ -48,6 +40,20 @@ if (isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
 }
+?>
+<!-- post -->
+<?php
+include 'connection.php'; 
+$query = "SELECT p.postId, p.postTitle, p.postContent, p.postTag, u.firstName, u.lastName FROM post p INNER JOIN user u ON p.postUserId = u.userId ORDER BY p.postDate DESC";
+$result = $conn->query($query);
+$posts = [];
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $posts[] = $row;
+    }
+}
+$conn->close();
 ?>
 
 <body>
@@ -61,7 +67,8 @@ if (isset($_SESSION['user_id'])) {
             <a href="post.php">Make Post</a>
             <div class="info">
                 <a href="profile.php">My Profile</a>
-                <img src="../Images/test.jpg" alt="Avatar">
+                <img src="<?php echo $iconData ? 'data:image/jpeg;base64,' . $iconData : '../Images/profile.jpg'; ?>"
+                    id="avatarImage" />
                 <div class="dropdown-content">
                     <a href="profile.php">Profile</a>
                     <a href="logout.php">Logout</a>
@@ -79,33 +86,41 @@ exit();
             </div>
         </div>
     </div>
-    <div class="post">
-        <p class="title">blablabla</p>
-        <img src="../Images/th.jpg" />
-        <div class="context">
-            <?php
-if (isset($_SESSION['user_id'])) {
-$userId = $_SESSION['user_id'];
-
-echo "<div class='ses'>Welcome, " . $firstName . " " . $lastName . "</div>";
-} else {
-header("Location: login.php");
-exit();
-}
-?>
+    <?php foreach ($posts as $row): ?>
+    <div class="post" title="Click for more details">
+        <p class="title"><?= htmlspecialchars($row['postTitle']); ?></p>
+        <img src="../Images/th.jpg" alt="Thumbnail">
+        <div class="context"><?php
+            $postContent = htmlspecialchars($row['postContent']);
+            if (strlen($postContent) > 500) {
+                // if exceed 500 then ... for the rest
+                echo substr($postContent, 0, 500) . "......";
+            } else {
+                echo $postContent;
+            }
+            ?>
         </div>
-        <!-- <div class="comment">
-            <input type="comment" placeholder="Leave a comment...">
-            <a href="#"><i class="fa-solid fa-envelope"></i></a>
-        </div> -->
         <div class="user">
-            <img src="../Images/test.jpg" id="test" />
-            <p class="username" id="username">smith</p>
-            <a href="#"><i class='far fa-thumbs-up' id="thumbsup" onclick="toggleLike()"></i></a>
-            <a href="#"><i class='fas fa-thumbs-up' id="thumbsup1" style="display:none;" onclick="toggleLike()"></i></a>
-            <!-- <p class="count" id="count">123</p> -->
+            <img src="<?php echo $iconData ? 'data:image/jpeg;base64,' . $iconData : '../Images/profile.jpg'; ?>"
+                id="avatarImage" />
+            <p class="username" id="username">
+                <?= htmlspecialchars($row['firstName']) . " " . htmlspecialchars($row['lastName']); ?></p>
+            <a href="#" title="Like"><i class='far fa-thumbs-up' id="thumbsup"
+                    onclick="toggleLike(event, this)"></i></a>
+            <a href="#" title="Unlike"><i class='fas fa-thumbs-up' id="thumbsup1" style="display:none;"
+                    onclick="toggleLike(event, this)"></i></a>
         </div>
     </div>
+    <?php endforeach; ?>
+    <script>
+    function toggleLike(event, element) {
+        event.stopPropagation();
+        var like = element.id === "thumbsup" ? "thumbsup" : "thumbsup1";
+        var oppositeLike = element.id === "thumbsup" ? "thumbsup1" : "thumbsup";
+        element.style.display = "none";
+        document.getElementById(oppositeLike).style.display = "inline-block";
+    }
+    </script>
 
     <div class="navBar">
         <p>Tags/Categories</p>
