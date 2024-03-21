a182006eb1a773c28c241850999f8eab2c55778b
GorgeousGrace <shangqianyu2018@126.com>
2024-03-21 15:59:51 -0700

    Add Back to Top button on the main page

--- PHP/main.php
+++ PHP/main.php
@@ -11,18 +11,37 @@
     <script src="https://kit.fontawesome.com/d1344ce34d.js" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/0485a9f289.js" crossorigin="anonymous"></script>
 
+    <script>
+        window.onscroll = function() {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("backToTopButton").style.display = "block";
+            } else {
+                document.getElementById("backToTopButton").style.display = "none";
+            }
+        }
+
+        document.getElementById("backToTopButton").onclick = function() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+    </script>
+
 </head>
 <!-- profile -->
 <?php
 session_start();
-include 'connection.php'; 
+include 'connection.php';
 if (isset($_SESSION['user_id'])) {
     $userId = $_SESSION['user_id'];
     $stmt = $conn->prepare("SELECT firstName, lastName, emailAddress, icon FROM user WHERE userId = ?");
     $stmt->bind_param("i", $userId); // 'i' indicates the type is integer
     $stmt->execute();
     $result = $stmt->get_result();
-    
+
     if ($result->num_rows > 0) {
         $user = $result->fetch_assoc();
         $firstName = htmlspecialchars($user['firstName']);
@@ -30,7 +49,7 @@ if (isset($_SESSION['user_id'])) {
         if ($user['icon']) {
             $iconData = base64_encode($user['icon']);
         } else {
-            $iconData = ''; 
+            $iconData = '';
         }   // Convert the icon data to a data URI
     } else {
         echo "No user found.";
@@ -43,13 +62,13 @@ if (isset($_SESSION['user_id'])) {
 ?>
 <!-- post -->
 <?php
-include 'connection.php'; 
+include 'connection.php';
 $query = "SELECT p.postId, p.postTitle, p.postContent, p.postTag, u.firstName, u.lastName FROM post p INNER JOIN user u ON p.postUserId = u.userId ORDER BY p.postDate DESC";
 $result = $conn->query($query);
 $posts = [];
 
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
         $posts[] = $row;
     }
 }
@@ -57,6 +76,9 @@ $conn->close();
 ?>
 
 <body>
+
+    <a href="#top" id="backToTopButton" title="Go to top"> TOP </a>
+
     <div class="sidebar">
         <a href="#"><i class="fa-solid fa-house-chimney">&nbsp;Home</i></a>
         <div class="search-bar">
@@ -67,59 +89,55 @@ $conn->close();
             <a href="post.php">Make Post</a>
             <div class="info">
                 <a href="profile.php">My Profile</a>
-                <img src="<?php echo $iconData ? 'data:image/jpeg;base64,' . $iconData : '../Images/profile.jpg'; ?>"
-                    id="avatarImage" />
+                <img src="<?php echo $iconData ? 'data:image/jpeg;base64,' . $iconData : '../Images/profile.jpg'; ?>" id="avatarImage" />
                 <div class="dropdown-content">
                     <a href="profile.php">Profile</a>
                     <a href="logout.php">Logout</a>
                 </div>
                 <?php
-if (isset($_SESSION['user_id'])) {
-$userId = $_SESSION['user_id'];
+                if (isset($_SESSION['user_id'])) {
+                    $userId = $_SESSION['user_id'];
 
-echo "<div class='ses'>Welcome, " . $firstName . " " . $lastName . "</div>";
-} else {
-header("Location: login.php");
-exit();
-}
-?>
+                    echo "<div class='ses'>Welcome, " . $firstName . " " . $lastName . "</div>";
+                } else {
+                    header("Location: login.php");
+                    exit();
+                }
+                ?>
             </div>
         </div>
     </div>
-    <?php foreach ($posts as $row): ?>
-    <div class="post" title="Click for more details">
-        <p class="title"><?= htmlspecialchars($row['postTitle']); ?></p>
-        <img src="../Images/th.jpg" alt="Thumbnail">
-        <div class="context"><?php
-            $postContent = htmlspecialchars($row['postContent']);
-            if (strlen($postContent) > 500) {
-                // if exceed 500 then ... for the rest
-                echo substr($postContent, 0, 500) . "......";
-            } else {
-                echo $postContent;
-            }
-            ?>
+    <?php foreach ($posts as $row) : ?>
+        <div class="post" title="Click for more details">
+            <p class="title"><?= htmlspecialchars($row['postTitle']); ?></p>
+            <img src="../Images/th.jpg" alt="Thumbnail">
+            <div class="context"><?php
+                                    $postContent = htmlspecialchars($row['postContent']);
+                                    if (strlen($postContent) > 500) {
+                                        // if exceed 500 then ... for the rest
+                                        echo substr($postContent, 0, 500) . "......";
+                                    } else {
+                                        echo $postContent;
+                                    }
+                                    ?>
+            </div>
+            <div class="user">
+                <img src="<?php echo $iconData ? 'data:image/jpeg;base64,' . $iconData : '../Images/profile.jpg'; ?>" id="avatarImage" />
+                <p class="username" id="username">
+                    <?= htmlspecialchars($row['firstName']) . " " . htmlspecialchars($row['lastName']); ?></p>
+                <a href="#" title="Like"><i class='far fa-thumbs-up' id="thumbsup" onclick="toggleLike(event, this)"></i></a>
+                <a href="#" title="Unlike"><i class='fas fa-thumbs-up' id="thumbsup1" style="display:none;" onclick="toggleLike(event, this)"></i></a>
+            </div>
         </div>
-        <div class="user">
-            <img src="<?php echo $iconData ? 'data:image/jpeg;base64,' . $iconData : '../Images/profile.jpg'; ?>"
-                id="avatarImage" />
-            <p class="username" id="username">
-                <?= htmlspecialchars($row['firstName']) . " " . htmlspecialchars($row['lastName']); ?></p>
-            <a href="#" title="Like"><i class='far fa-thumbs-up' id="thumbsup"
-                    onclick="toggleLike(event, this)"></i></a>
-            <a href="#" title="Unlike"><i class='fas fa-thumbs-up' id="thumbsup1" style="display:none;"
-                    onclick="toggleLike(event, this)"></i></a>
-        </div>
-    </div>
     <?php endforeach; ?>
     <script>
-    function toggleLike(event, element) {
-        event.stopPropagation();
-        var like = element.id === "thumbsup" ? "thumbsup" : "thumbsup1";
-        var oppositeLike = element.id === "thumbsup" ? "thumbsup1" : "thumbsup";
-        element.style.display = "none";
-        document.getElementById(oppositeLike).style.display = "inline-block";
-    }
+        function toggleLike(event, element) {
+            event.stopPropagation();
+            var like = element.id === "thumbsup" ? "thumbsup" : "thumbsup1";
+            var oppositeLike = element.id === "thumbsup" ? "thumbsup1" : "thumbsup";
+            element.style.display = "none";
+            document.getElementById(oppositeLike).style.display = "inline-block";
+        }
     </script>
 
     <div class="navBar">
